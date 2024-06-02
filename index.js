46deff86a29a6163511d7ca5a1026a252eb00e8d
bduber274 <132111278+bduber274@users.noreply.github.com>
2024-06-02 05:41:11 +0000

    a

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
-const port = "6969"
+const port = "8080"
 const Corrosion = require('./lib/server')
 const express = require('express')
 const firebase = require('firebase/app');
 require('firebase/auth'); 
-
-// Firebase initialization
-const firebaseConfig = {
-  apiKey: "AIzaSyDakr7a_ty2LcrECw0Bo48LlFKN2-tgIVI",
-  authDomain: "celestial-network.firebaseapp.com",
-  projectId: "celestial-network",
-  storageBucket: "celestial-network.appspot.com",
-  messagingSenderId: "668361817556",
-  appId: "1:668361817556:web:29baa7fba0e76fc1106a41",
-  measurementId: "G-CCWF3DG4W3"
-};
-
-// Initialize Firebase
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const app = express()
 
 const proxy = new Corrosion({
     prefix: "/service/",
     codec: "xor",
-    title: "Celestial Network",
+    title: "Nova Network",
     forceHttps: true,
     requestMiddleware: [
         Corrosion.middleware.blacklist([
             "accounts.google.com", "pornhub.com", "xvideos.com", "https://google.com/search?q=porn", "https://rule34.xxx"
-        ], "The page has been blocked by Nova Network to protect your security and the innocence of the soul."),
+        ], "The page has been blocked by Nova Network"),
     ]
 
 })
@@ -44,5 +30,5 @@ app.use('/', function (req, res) {
 });
 
 app.listen(process.env.PORT || port, () => {
-  console.log(`Hooray! Nova is running at localhost:${port}`)
+  console.log(`Nova is successfully running at localhost:${port}`)
 })
